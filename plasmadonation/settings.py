d77005133509164ac91d445f8378667f24fe4fb6
swojas <60619308+swojas@users.noreply.github.com>
2020-12-19 14:55:33 +0530

    Update settings.py

--- plasmadonation/settings.py
+++ plasmadonation/settings.py
@@ -125,7 +125,6 @@ USE_TZ = True
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
